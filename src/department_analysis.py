00e013e675afad854dbfa13939f3110d093e71b1
piyushyadav1605 <piyushyadav9748@gmail.com>
2026-09-05 19:27:13 +0530

    Improve department attrition visualization

--- src/department_analysis.py
+++ src/department_analysis.py
@@ -23,11 +23,20 @@ attrition_rate = attrition_rate.sort_values(ascending=False)
 for department, rate in attrition_rate.items():
     print(f"{department:<25} : {rate:.2f}%")
 plt.figure(figsize=(8,5))    
-plt.bar(attrition_rate.index, attrition_rate.values)
+bars = plt.bar(attrition_rate.index, attrition_rate.values)
 plt.title("Attrition Rate by Department")
 plt.xlabel("Department")
 plt.ylabel("Attrition Rate (%)")
+for bar in bars:
+    height = bar.get_height()
+    x=bar.get_x() + bar.get_width() / 2
+    y=height+1
+    plt.text(x, y, f"{height:.2f}%",)
+plt.xticks(rotation=45)
+plt.savefig("images/attrition_rate_by_department.png", )
+
 plt.show()
+
 
 
 
